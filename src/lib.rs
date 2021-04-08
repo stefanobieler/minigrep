5eb816980ed0391cbc733970db84ca6c167d77a4
Stefano Bieler <stefanobieler@gmail.com>
2021-04-08 15:32:11 -0700

    added a failing test

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,10 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
 
 }
 
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str>{
+    return vec![];
+}
+
 pub struct Config {
     pub query: String,
     pub filename: String
@@ -27,5 +31,21 @@ impl Config {
 
 
         return Ok(Config { query, filename });
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+            Rust:
+            safe, fast, productive.
+            Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive"], search(query, contents))
     }
 }
